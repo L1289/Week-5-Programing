92b55875db536ad68125713bface3252be9d7775
L1289 <lallsuk@sheridancollege.ca>
2025-02-07 15:17:30 -0500

    Made FleeAT
    Managed to get flee work

--- Assets/Scripts/Actions/FleeAT.cs
+++ Assets/Scripts/Actions/FleeAT.cs
@@ -1,11 +1,16 @@
 using NodeCanvas.Framework;
 using ParadoxNotion.Design;
+using TMPro;
 using UnityEngine;
 using UnityEngine.AI;
 
 namespace NodeCanvas.Tasks.Actions {
 
 	public class FleeAT : ActionTask {
+
+        public Transform targetTransform;
+        public BBParameter<Vector3> targetPosition;
+        public float fleeDistance; 
 
 		//Use for initialization. This is called only once in the lifetime of the task.
 		//Return null if init was successfull. Return an error string otherwise
@@ -22,8 +27,18 @@ namespace NodeCanvas.Tasks.Actions {
 
 		//Called once per frame while the action is active.
 		protected override void OnUpdate() {
-			
-		}
+
+            //What is the direction thaat we flee from?
+            Vector3 directionToTarget =  agent.transform.position - targetTransform.position;
+
+			//How far along that direction do we want to move?
+			if (directionToTarget.magnitude <= fleeDistance)
+			{
+				Vector3 target = fleeDistance * directionToTarget.normalized;
+				targetPosition.value = target;
+			}
+
+        }
 
 		//Called when the task is disabled.
 		protected override void OnStop() {
